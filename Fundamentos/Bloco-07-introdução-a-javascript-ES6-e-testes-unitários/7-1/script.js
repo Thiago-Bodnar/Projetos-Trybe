4e4d2518eafdcc64979ec545b0377537cfb2082a
Thiago <bodnarthiago@gmail.com>
2022-01-12 16:11:04 -0300

    exercício 1 feito

---
+++ Fundamentos/Bloco-07-introdução-a-javascript-ES6-e-testes-unitários/7-1/script.js
@@ -0,0 +1,13 @@
+const testingScope = escopo => escopo ? console.log(`Ótimo, fui ultilizada Dentro do escopo(if)`) : console.log(`Não devo ser ultilizada fora do meu escopo (else)`)
+//   if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
+
+testingScope(false);
